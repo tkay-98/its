f86141e9391b5300e3ad22c6481afc228b5958bb
tkay-98 <lt.tatsu32@gmail.com>
2023-09-05 22:30:47 +0900

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into issues (summary, description) values ('バグA', 'バグAがあります');
+insert into issues (summary, description) values ('バグB', 'バグBがあります');
+insert into issues (summary, description) values ('バグC', 'バグCがあります');
